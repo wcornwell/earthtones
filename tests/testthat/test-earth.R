b1ea09f73f93ec60f1ecf82222ddb3bee97f1859
wcornwell <wcornwell@gmail.com>
2016-08-28 20:59:11 +1000

    more testing

--- tests/testthat/test-earth.R
+++ tests/testthat/test-earth.R
@@ -1,18 +1,20 @@
-context("earthtone")
+context("earthtones")
 
 ## Need to do something nice here with testing?
 test_that("returns normal", {
   expect_is(get_earthtones(),"palette")
+  expect_is(get_earthtones(method="kmeans"),"palette")
   expect_null(print(get_earthtones()))
+  expect_is(get_earthtones(include.map=FALSE),"character")
+  expect_message(get_earthtones(sampleRate = 250))
 })
 
 test_that("test stop() errors", {
-  expect_error(expr = earthtones::get_earthtones(method="42"), message = "method.*")
+  expect_error(earthtones::get_earthtones(method="42"), message = "hello method.*")
   expect_error(expr = if (!requireNamespace("cluster",quietly=TRUE)) {
     earthtones::get_earthtones(method="pam")
     } else {
       stop("method")
     },
     message = "method.*")
-  expect_message(get_earthtones(sampleRate = 250))
 })
